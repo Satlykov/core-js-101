7cfcb5d45770cfd504403307c82cefdc2bfe0335
Rustam Satlykov <satlikov.rustam.mail@gmail.com>
2021-12-29 03:58:05 +0500

    feat: add solutions for 02-num-tasks.js

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -134,10 +134,9 @@ function getAngleBetweenVectors(x1, y1, x2, y2) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
-  throw new Error('Not implemented');
+function getLastDigit(value) {
+  return +(`${value}`.split('')[`${value}`.length - 1]);
 }
-
 
 /**
  * Returns a number by given string representation.
@@ -150,8 +149,8 @@ function getLastDigit(/* value */) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(/* value */) {
-  throw new Error('Not implemented');
+function parseNumberFromString(value) {
+  return +value;
 }
 
 /**
@@ -167,8 +166,8 @@ function parseNumberFromString(/* value */) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelepipedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getParallelepipedDiagonal(a, b, c) {
+  return (a ** 2 + b ** 2 + c ** 2) ** 0.5;
 }
 
 
@@ -189,8 +188,19 @@ function getParallelepipedDiagonal(/* a, b, c */) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  const arr = String(num).split('').map((el) => +el);
+
+  if (pow !== 0) {
+    if (arr[arr.length - 1 - pow] >= 5) {
+      arr[arr.length - pow - 1] += 1;
+    }
+    for (let i = (arr.length - pow); i < arr.length; i += 1) {
+      arr[i] = 0;
+    }
+  }
+
+  return +arr.join('');
 }
 
 /**
